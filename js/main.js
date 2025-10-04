42af0d016c6d3beb6b909ddc9151e5c3297ecb09
fawi-stores <fawiboyoo@gmail.com>
2025-10-04 01:32:36 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // 1. Sticky Header & Shrink on Scroll
+    const header = document.querySelector('.main-header');
+    const heroSection = document.querySelector('.hero-section');
